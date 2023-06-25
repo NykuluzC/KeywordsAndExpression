3c9cdfad1fab1c38f7407ba459d8c99a22805a6a
Nykuluz <carlmillanes3@gmail.com>
2023-06-26 04:08:21 +0800

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args){
+        double miles = 100; // expression
+        double kilometers = (miles * 1.609344); //expression
+
+        int highScore = 50; //expression
+
+        if(highScore > 25){ //expression
+            highScore += 1000; //add bonus points expression
+        }
+
+        int health = 100; // 1 expression
+
+        if((health < 25) && (highScore > 1000)) { //3 expressions
+            highScore = highScore - 1000; //2 expressions
+        }
 
     }
 }
